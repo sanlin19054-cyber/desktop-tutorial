46e0c71e03f6688428de0d5048351527be4a7146
遊客15360654132 <a0979300943@gmail.com>
2026-08-13 21:13:58 +0800

    Track和PID
    更新了小車的尋跡功能，加入了PID（P 負責即時修正，I 負責消除長期偏差，D 負責抑制轉彎後的左右震盪），控制結構為四路循迹->计算 error->PID ->修正量 correction ->左右轮差速 -> MOTOR。

--- Track/track.h
+++ Track/track.h
@@ -1,7 +1,7 @@
 #ifndef __TRACK_H
 #define __TRACK_H
 
-#include "main.h" 
+#include "main.h"
 
 /*==========================================================
   四路循迹传感器
@@ -24,15 +24,11 @@
 #define TRACK_R1_PORT    GPIOB
 #define TRACK_R1_PIN     GPIO_PIN_11
 
-
 /* 初始化 */
 void TRACK_Init(void);
 
 /* 读取四路传感器 */
-void TRACK_Read(uint8_t *L1,
-                uint8_t *L2,
-                uint8_t *R2,
-                uint8_t *R1);
+void TRACK_Read(uint8_t *L1, uint8_t *L2, uint8_t *R2, uint8_t *R1);
 
 /* 执行一次循迹控制 */
 void TRACK_Run(void);
